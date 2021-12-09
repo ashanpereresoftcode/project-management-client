e452e154c87503f792e3f849e353641f23da5310
ashanperera201 <AshanPerera.201@gmail.com>
2021-12-10 00:50:58 +0530

    changes.

--- src/app/dashboard/project-management/project-resource-allocation/project-resource-allocation.component.ts
+++ src/app/dashboard/project-management/project-resource-allocation/project-resource-allocation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
 import { ToastrService } from 'ngx-toastr';
+import { MatTableDataSource } from '@angular/material/table';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ProjectManagementService, AuthService } from '../../../shared/services';
 
@@ -12,40 +13,37 @@ import { ProjectManagementService, AuthService } from '../../../shared/services'
 export class ProjectResourceAllocationComponent implements OnInit {
 
   @BlockUI() blockUI!: NgBlockUI;
-  projects: any = [];
+  displayedColumns: string[] = ['first-name', 'last-name', 'designation'];
+  dataSource: any = new MatTableDataSource<any>();
 
   constructor(
     public matDialogRef: MatDialogRef<ProjectResourceAllocationComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private projectManagementService: ProjectManagementService,
     private authService: AuthService,
-    private toastrService: ToastrService) { }
+    private toastrService: ToastrService) {
+    this.matDialogRef.disableClose = true;
+  }
 
   ngOnInit(): void {
     this.blockUI.start('Fetching .....');
     this.authService.fetchUsers().subscribe(serviceRes => {
       if (serviceRes) {
         const project = this.data?.project;
-        this.projects = serviceRes.result;
-        // user -> assigned-projects -> selected-project-id
-        debugger
+        const assignedUsers = serviceRes.result.filter((x: any) => x.assignedProjects && x.assignedProjects.length > 0);
+        this.dataSource.data = assignedUsers.filter((u: any) => {
+          return u.assignedProjects.filter((ap: any) => ap.project._id === project._id)
+        })
       }
       this.blockUI.stop();
     }, error => {
       this.blockUI.stop();
       console.log(error);
-    })
-    // this.projectManagementService.getAssignedProjects().subscribe(projects => {
-    //   if (projects) {
-    //     const project = this.data?.project;
-    //     this.projects = projects.result;
-    //     debugger
-    //   }
-    //   this.blockUI.stop();
-    // }, error => {
-    //   this.blockUI.stop();
-    //   console.log(error);
-    // })
+    });
+  }
+
+  closeModal = () => { 
+    this.matDialogRef.close();
   }
 
 }
